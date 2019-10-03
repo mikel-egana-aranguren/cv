7346a6a509bd678b0e8376c4582c89ff014d93f3
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2019-10-03 09:47:01 +0200

    Deusto, docker not working yet

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM ubuntu:18.04
 RUN apt-get update && apt-get install texlive-latex-base -y
-COPY . /app
-CMD pdflatex /app/mikel_egana_aranguren_cv.tex
+#COPY . /app
+#CMD pdflatex /app/mikel_egana_aranguren_cv.tex
+#CMD bibtex /app/mikel_egana_aranguren_cv
+#CMD pdflatex /app/mikel_egana_aranguren_cv.tex
+CMD ["sleep", "1d"]
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+# ACHTUNG BITE! doesn't work yet
+
+
+# TODO: push image to repo
+# TODO: add script that is listening always, and whenever a *.tex file is added, compiles it and copies it to the right dir, if necessary
+# TODO: check *.bib and perl scripts for new papers
+
 docker run -d -v /home/mikel/cv:/cv mikeleganaaranguren/cv:latest
 docker exec -it vibrant_bardeen pdflatex /cv/mikel_egana_aranguren_cv.tex
 docker exec -it vibrant_bardeen cp mikel_egana_aranguren.pdf /cv
